586344e7474dc4eb8cc1026322edcdd21746d790
hitohiratama <56036158+hitohiratama@users.noreply.github.com>
2024-01-20 22:43:03 +0900

    Update Form1.cs

--- 素数フォーム/Form1.cs
+++ 素数フォーム/Form1.cs
@@ -6,6 +6,8 @@ namespace 素数フォーム
         {
             InitializeComponent();
         }
+         /* 素数判定するメソッド.
+ 判定3番目の(Input_date + 2) % 6 == 1の"＋２"は(Input_date % 6 == 1　と同じ式にするためにある。*/
         private void PrimeNum_function(int Input_date)
         {
             if (Input_date == 2 || Input_date == 3)
@@ -29,7 +31,8 @@ namespace 素数フォーム
                 Output_data.Text = (Input_date + "は素数ではないです。");
             }
         }
-
+ // フォームからデータを受け取る.
+ //strからintに変換
         private void Primenum_check_Click(object sender, EventArgs e)
         {
             Int32.TryParse(Input_date.Text, out int Anwer);
